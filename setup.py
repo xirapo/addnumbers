d6ebb9c51fecbcbb8edb4b552f6d537dc4dc4997
xirapo <fgarciadiaz29@gmail.com>
2021-03-14 17:21:55 -0600

    update package name

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from setuptools import setup as s, find_packages
 
 s(
-    name='add_2_numbers',
-    version='0.0.1',
+    name='sum',
+    version='0.1',
     description='Testing packages',
     author='francisco garcia',
     url='https://github.com/xirapo/add2numbers.git',
