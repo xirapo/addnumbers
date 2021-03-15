47e3593833e3c577680e0c063a5956f5def2c165
xirapo <fgarciadiaz29@gmail.com>
2021-03-14 22:42:00 -0600

    arguments

--- addnumbers/post_install.py
+++ addnumbers/post_install.py
@@ -1,8 +1,9 @@
-import pathlib, os
+import pathlib, os, sys
 
 current =pathlib.Path(__file__).parent.absolute()
 files =os.listdir(current)
-def install(pth):
+def install():
+    pth = sys.argv[1]
     print("Running Post Install...")
     print(f"new folder path: {pth}")
     for f in files:
