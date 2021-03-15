0fa48cc43f8578a3782ee78eb2f403e710bae884
xirapo <fgarciadiaz29@gmail.com>
2021-03-14 23:00:27 -0600

    moving .py file from resolver to gateway

--- addnumbers/scripts/post_install.py
+++ addnumbers/scripts/post_install.py
@@ -1,12 +1,13 @@
-import pathlib, os, sys
+import pathlib, os, sys, shutil
 
 current =pathlib.Path(__file__).parent.absolute()
-files =os.listdir(current)
+list_of_files =os.listdir(current)
 
 def install():
     pth = sys.argv[1]
     print("Running Post Install...")
     print(f"new folder path: {pth}")
-    for f in files:
-        if f.endswith('.py'):
-            print(f)
+    for root, dirs, files in os.walk(current):
+        for f in files:
+            if f.endswith('.py'):
+                shutil.copy(os.path.join(root,f),pth)
