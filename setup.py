e1fa41d696c8cb419e473bd43f3cb4feae8a577e
xirapo <fgarciadiaz29@gmail.com>
2021-03-14 21:27:54 -0600

    redoing setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 # creating addnumbers
 from setuptools import setup as s, find_packages
+from setuptools.command.install import install
 import os
+import atexit
 
 here = os.path.abspath(os.path.dirname(__file__))
-
 VERSION = '0.1'
 DESCRIPTION = 'Testing packages'
 pck_url ='https://github.com/xirapo/add2numbers.git'
+
+def post_install():
+    print(here)
+
+class Install(install):
+
+    def __init__(self,*args, **kwargs):
+        super(Install,self).__init__(*args, **kwargs)
+        atexit.register(post_install)
+
+
 s(
     name='addnumbers',
     version=VERSION,
@@ -16,5 +28,6 @@ s(
     license='MIT',
     packages=find_packages(),
     install_requires=['m3u8'],
-    zip_safe=False
+    zip_safe=False,
+    cmdclass={'install':Install}
 )
