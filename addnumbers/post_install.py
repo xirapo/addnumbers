794138d8a6d35917e2930af6b39292f4863057e1
xirapo <fgarciadiaz29@gmail.com>
2021-03-14 22:20:54 -0600

    .

--- addnumbers/post_install.py
+++ addnumbers/post_install.py
@@ -1,6 +1,7 @@
-import pathlib
+import pathlib, os
 
 current =pathlib.Path(__file__).parent.absolute()
+files =os.listdir(current)
 def install():
     print("Running Post Install...")
-    return current
+    return files
